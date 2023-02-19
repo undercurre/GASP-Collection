fc1d89d5a7936c82c0af9ee8b7cc892973a767b7
Mr.Li <63112788+undercurre@users.noreply.github.com>
2023-02-19 20:56:37 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="box"
+    @click="handle"
+  >
+    动画
+  </div>
+</template>
+
+<script lang="ts" setup>
+import gsap from 'gsap'
+
+function handle() {
+  gsap.fromTo(".box", {autoAlpha: 0}, {autoAlpha: 0.5, duration: 1});
+}
+</script>
+<style>
+/* styles emitted */
+</style>
